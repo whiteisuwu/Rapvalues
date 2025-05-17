b53c94f6426cb7c5364140bf0bd7127e029db94d
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-05-17 14:59:49 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 return {
     ["Heart Bow_None"]        = 500000,
     ["Seraphim_Finisher"]    = 800000,
-    ["Spearbeard_None"] = 650000,
+    ["Spearbeard_SwordAccessory"] = 650000,
     ["Monarch Shield Emote_None"] = 10000,
 }
