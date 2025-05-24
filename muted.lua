c104caa43d2e92055381d74d669b6d4b71a35748
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-05-24 16:14:27 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -2,6 +2,5 @@ return {
     ["Shark_None"] = true,
     ["Snow Globe_None"] = true,
     ["Nebula Blade_None"] = true,
-    ["Chroma Ninja Katana_Finisher"] = true,
     ["Yin Yang Scythe_None"] = true,
 }
