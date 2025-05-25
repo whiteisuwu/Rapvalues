707f9204bdc554cb490b107556d085396df7dc63
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-05-25 14:14:06 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -1,6 +1,7 @@
 return {
     "Bubble Blaster_None",
     "Dual Leviathan Set_None",
+    "Green Ninja Katana_Finisher",
     "Nebula Blade_None",
     "Serpent's Cataclysm_None",
     "Shark_None",
