da2818b2d785b8b084e7b3898b95d8d8529ed6e4
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-05-25 15:24:29 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -4,6 +4,7 @@ return {
     "Bubble Blaster_None",
     "Chroma Ninja Katana_Finisher",
     "Dual Leviathan Set_None",
+    "Dual Loveclaw Blade_None",
     "Dual Nebula Blasters_Finisher",
     "Dual Nebula Blasters_None",
     "Green Ninja Katana_Finisher",
