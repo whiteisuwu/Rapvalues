901472d91d14d67bf9a8e66aa6e0ca9adb30b402
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-03 16:39:47 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -10,6 +10,7 @@ return {
     "Hollow Oath Katana_None",
     "Kraken Pit_None",
     "Paw Punch_None",
+    "Pink Ninja Katana_Finisher",
     "Pink Ninja Katana_None",
     "Resurrection Scythe_None",
     "Serpent's Cataclysm_None",
