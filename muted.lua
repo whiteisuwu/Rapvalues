bd54e6d770fc123daca0c12bc9ae99b9c237610f
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-05 01:00:27 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -2,6 +2,7 @@ return {
     "Cat Paw Emote_None",
     "Chroma Ninja Katana_Finisher",
     "Floppy Chicken_None",
+    "Hollow Oath Katana_None",
     "Paw Punch_None",
     "Thorned Sovereign_None",
 }
