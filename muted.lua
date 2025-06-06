e3a74a86ac491f5afebe5fa82bd3fddd97f52bc4
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-06 16:20:52 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -2,6 +2,7 @@ return {
     "Cat Paw_None",
     "Chroma Blade Explosion_None",
     "Floppy Chicken_None",
+    "Paw Punch_None",
     "Serpent's Cataclysm_None",
     "Serpent_None",
     "Yin Yang Scythe_None",
