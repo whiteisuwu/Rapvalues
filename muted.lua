51184452801bd79dd5c9225e168b023b5c714135
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-06 19:36:20 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -4,6 +4,7 @@ return {
     "Cat Paw_None",
     "Chroma Blade Explosion_None",
     "Chroma Ninja Katana_Finisher",
+    "Dual Nebula Blasters_Finisher",
     "Eclipse Gleam_None",
     "Floppy Chicken_None",
     "Forsaken Riftide_None",
