5fc8a6d83a622a602c8de6f04537f1f5862fb29f
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-15 14:39:46 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -2,6 +2,7 @@ return {
     "Astraea Staff_None",
     "Black Ninja Katana_Finisher",
     "Dual Purity Blade_None",
+    "Final Phase_None",
     "Forsaken Riftide_None",
     "Oblivion Pike_None",
     "Paw Punch_None",
