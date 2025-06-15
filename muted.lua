92a24cfc4550625715f8dac4d846050edef225cb
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-15 14:09:49 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -3,6 +3,7 @@ return {
     "Black Ninja Katana_Finisher",
     "Dual Purity Blade_None",
     "Forsaken Riftide_None",
+    "Pure Power_None",
     "Purity Blade_None",
     "Shatterflight Bird_None",
     "Yin Yang Scythe_None",
