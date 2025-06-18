2bbef091623dea4f8d7ad57cf0b62294cd6d26cb
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-18 17:51:41 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -5,6 +5,7 @@ return {
     "Dual Nebula Blasters_None",
     "Dual Purity Blade_None",
     "Final Phase_None",
+    "Forsaken Riftide_None",
     "Frostbound Lantern_None",
     "Hollow Oath Katana Emote_None",
     "Hollow Oath_None",
