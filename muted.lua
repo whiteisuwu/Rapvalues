165adfdc2f2af59c50de699b1f5389596fc6db60
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-20 01:00:34 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -3,6 +3,7 @@ return {
     "Chroma Ninja Katana_Finisher",
     "Final Phase_None",
     "Frostbound Lantern_None",
+    "Hollow Oath Katana Emote_None",
     "Noob_None",
     "Purity Blade_None",
     "Serpent's Cataclysm_None",
