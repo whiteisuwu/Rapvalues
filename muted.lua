ac5d01a972e94bed0f5ab5e13a31b2e9271a9b29
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-21 16:49:17 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -20,6 +20,7 @@ return {
     "Nebula Blade_None",
     "Noob_None",
     "Pink Ninja Katana_None",
+    "Polar Bear Pop_None",
     "Pure Power_None",
     "Purity Blade_None",
     "Resurrection Scythe Emote_None",
