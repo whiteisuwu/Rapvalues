80a745d6ddc5235719cb3f9f588d4d6220c40ebf
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-06-27 23:48:13 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -2,6 +2,8 @@ return {
     "Blossom Blade_None",
     "Calamity Guardian Emote_None",
     "Paw Punch_None",
+    "Prismatic Odachi Emote_None",
     "Prismatic Odachi Explosion_None",
     "Shark_None",
+    "Spinalis_None",
 }
