2f616ea166e1c96bcc9a71700a00c5de2d76b2bd
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-07-01 12:23:28 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -2,8 +2,10 @@ return {
     "Calamity Guardian Emote_None",
     "Calamity Guardian_None",
     "Dual Soulbloom Blade_None",
+    "Forsaken Riftide_None",
     "Hollow Oath Katana_None",
     "Paw Punch_None",
     "Soulbloom Blade_None",
     "Soulbloom Scissors_None",
+    "Spinalis_None",
 }
