822abfb6d9da754ee18e147b9464935ba8054be9
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-07-03 17:59:37 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -2,6 +2,7 @@ return {
     "Blossom Blade_None",
     "Calamity Guardian Emote_None",
     "Calamity Guardian_None",
+    "Chroma Ninja Katana_Finisher",
     "Dual Soulbloom Blade_None",
     "Forsaken Riftide_None",
     "Hollow Oath Katana_None",
