fd6bcdbd3c340f73c2320bf73f6ba74e2e14ac12
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-07-03 21:07:37 +0100

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -6,6 +6,7 @@ return {
     "Dual Soulbloom Blade_None",
     "Forsaken Riftide_None",
     "Hollow Oath Katana_None",
+    "Nebula Blade_None",
     "Paw Punch_None",
     "Soulbloom Blade_None",
     "Soulbloom Scissors_None",
