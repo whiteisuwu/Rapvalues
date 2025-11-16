bad2f5b1ef890f7a2f3083921ac05c4342641cbc
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-11-16 03:20:09 +0000

    Merge batched RAP updates

--- RapValues.lua
+++ RapValues.lua
@@ -3418,6 +3418,6 @@ return {
     ["Zodiac Parasol_None"] = 401,
     ["Zodiac Shrine_None"] = 38,
     ["Zombie Annihilation_None"] = 5,
-    ["Zombie Leg_None"] = 262,
+    ["Zombie Leg_None"] = 258,
     ["Zombie Sword_None"] = 0,
 }
