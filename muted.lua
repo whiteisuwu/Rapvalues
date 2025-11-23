a14a94bf603081936ffb3a3f594ba556070ee428
whiteisuwu <102001853+whiteisuwu@users.noreply.github.com>
2025-11-23 20:53:39 +0000

    Update muted.lua

--- muted.lua
+++ muted.lua
@@ -4,6 +4,7 @@ return {
     "Corrupt Blossom Bow_None",
     "Dual Corrupt Blossom Blade_None",
     "Nebula Blade_None",
+    "Shadowcleaver_None",
     "Snow Globe_None",
     "Violet Crusher_None",
 }
