6cd25175f5b4b66cc18f585ac51a872af49f6ec4
Trades <rapbot@example.com>
2026-04-24 17:46:45 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3550,7 +3550,7 @@ return {
     ["Regal  Burst_None"] = 4,
     ["Regalia Blade_None"] = 9,
     ["Regret Blades Explosion_None"] = 464,
-    ["Regret Blades_None"] = 385,
+    ["Regret Blades_None"] = 8000,
     ["Regret_None"] = 0,
     ["Reindeer Blade_None"] = 33,
     ["Reindeer Hammer_None"] = 109,
