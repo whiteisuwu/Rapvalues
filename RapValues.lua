abcab1cce93331c083c8e990d88f5dadbdcf5c5c
Trades <rapbot@example.com>
2026-04-26 03:20:56 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -2043,7 +2043,7 @@ return {
     ["Frostreaver_None"] = 0,
     ["Frostshard Blade_None"] = 4000000,
     ["Frostveil Twins_None"] = 0,
-    ["FROSTWALL_None"] = 9834,
+    ["FROSTWALL_None"] = 15000,
     ["Frostwraith_None"] = 7,
     ["Frosty Breath_None"] = 4,
     ["Frosty Clap_None"] = 4,
