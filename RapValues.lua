ff522925dbcac4f631aa529c53c509d0bd0d01ba
Trades <rapbot@example.com>
2026-04-26 03:20:56 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -2922,7 +2922,7 @@ return {
     ["Moonflower Greatsword_None"] = 3499,
     ["Moonflower Katana Emote_None"] = 217,
     ["Moonflower Katana_Finisher"] = 72652,
-    ["Moonflower Katana_None"] = 14800,
+    ["Moonflower Katana_None"] = 17000,
     ["Moonlight Blade_None"] = 79,
     ["Moonlight Bliss_None"] = 22,
     ["Moonlight Bow_None"] = 62,
