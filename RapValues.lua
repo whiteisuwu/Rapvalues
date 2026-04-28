e8e5870f92628545908cce591a063309235ba030
Trades <rapbot@example.com>
2026-04-28 20:41:22 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4170,7 +4170,7 @@ return {
     ["Starlight Axe_None"] = 97,
     ["Starlight Saber_None"] = 9,
     ["Starlight Scythe Emote_None"] = 54,
-    ["Starlight Scythe_None"] = 2083,
+    ["Starlight Scythe_None"] = 400,
     ["Starlight Shardblade_None"] = 5,
     ["Starlight Spear_None"] = 11,
     ["Starlit Bloom_None"] = 45,
