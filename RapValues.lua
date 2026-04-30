785b3558dd69851a43edaa76269810df103c6a03
Trades <rapbot@example.com>
2026-04-30 06:54:04 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -1033,7 +1033,7 @@ return {
     ["Diamond Dagger_None"] = 0,
     ["Diamond Slicer_None"] = 18,
     ["Diamond Spear_None"] = 90,
-    ["Diamond Starblade_None"] = 11238,
+    ["Diamond Starblade_None"] = 1000,
     ["Diamond Stars_None"] = 50,
     ["Diamond Sword_None"] = 4,
     ["Digital Breakdance_None"] = 26,
