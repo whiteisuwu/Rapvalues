5ef8f845bd297aac0a8b8aaa88c82ac391e93640
Trades <rapbot@example.com>
2026-04-30 15:04:19 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3873,7 +3873,7 @@ return {
     ["Shattered Peach_None"] = 156,
     ["Shattered Sword_None"] = 255,
     ["Shatterflight Bird Emote_None"] = 473,
-    ["Shatterflight Bird Explosion_None"] = 3100,
+    ["Shatterflight Bird Explosion_None"] = 4147,
     ["Shatterflight Bird_None"] = 7226,
     ["Shatterstorm_None"] = 8,
     ["Shatterthorn_None"] = 8,
