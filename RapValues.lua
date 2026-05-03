ead4cd7c74c5e5b88551796f5508820ceea70af0
Trades <rapbot@example.com>
2026-05-03 16:35:58 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -531,7 +531,7 @@ return {
     ["Bunny Imitation_None"] = 154,
     ["Bunny Staff_None"] = 7775,
     ["Bunny_Finisher"] = 250943,
-    ["Bunny_None"] = 52914,
+    ["Bunny_None"] = 135000,
     ["Bunny's Apprenticeblade_None"] = 36,
     ["Bunny's Edge_None"] = 65,
     ["bunnylimdaggerold_None"] = 0,
