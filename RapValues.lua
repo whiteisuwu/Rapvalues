96ed11e455da2f2dd864818ff14d0673d48390ae
Trades <rapbot@example.com>
2026-05-06 01:32:19 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -1250,7 +1250,7 @@ return {
     ["Dual Depths Blaster_None"] = 122,
     ["Dual Desert Blade Emote_None"] = 36,
     ["Dual Desert Blade_None"] = 185,
-    ["Dual Devil Katana_None"] = 650,
+    ["Dual Devil Katana_None"] = 1806,
     ["Dual Divine Blaster Emote_None"] = 28,
     ["Dual Divine Blaster_None"] = 500,
     ["Dual Divine Sword Emote_None"] = 35,
