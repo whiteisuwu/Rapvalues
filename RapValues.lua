bfbdd0c96634f34b123c7e6d70c03974cdae4320
Trades <rapbot@example.com>
2026-05-06 09:43:51 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3043,7 +3043,7 @@ return {
     ["New Years Greatsword_None"] = 1199607,
     ["New Years Slicer_None"] = 4876,
     ["New Years Staff_None"] = 12,
-    ["Nifty's Blade_None"] = 10195,
+    ["Nifty's Blade_None"] = 2000,
     ["Night Raver_None"] = 1293,
     ["Nightblade_None"] = 3,
     ["Nightclaw Blade_None"] = 63,
