a5943b8325aeb88c7fd3441f63b1359f1cdeaecc
Trades <rapbot@example.com>
2026-05-11 01:10:53 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3689,7 +3689,7 @@ return {
     ["Rose Bow Emote_None"] = 83,
     ["Rose Bow_None"] = 370,
     ["Rose Gas_None"] = 79,
-    ["Rose Gift_None"] = 9000,
+    ["Rose Gift_None"] = 8558,
     ["Rose Greatsword_None"] = 1000000,
     ["Rose Mirage Bloom_None"] = 45,
     ["Rose Petal Rupture_None"] = 11,
