f2b6e50cebc56eb6c822b9282654a6df0dad37ab
Trades <rapbot@example.com>
2026-05-13 02:20:05 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4680,7 +4680,7 @@ return {
     ["Waterplosion_None"] = 18977,
     ["Wave Rider_None"] = 11,
     ["Wavelight Greatblade_None"] = 76,
-    ["Wavelight_None"] = 753,
+    ["Wavelight_None"] = 400,
     ["We're Doomed_None"] = 3,
     ["Wealth Cutter_None"] = 2,
     ["Web Slinger_None"] = 20,
