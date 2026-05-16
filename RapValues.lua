acedc2dbdff4c31386848e0d51e2fc23d68d7c34
Trades <rapbot@example.com>
2026-05-16 18:41:58 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -1005,7 +1005,7 @@ return {
     ["Dawnstep Edge_None"] = 15,
     ["Deadeye Glacier_None"] = 7,
     ["Deadman's Edge_None"] = 7,
-    ["Death Call Blade_None"] = 502,
+    ["Death Call Blade_None"] = 350,
     ["Death Call Scythe_None"] = 55,
     ["Death Call Timer_None"] = 38,
     ["Deathpiercer_None"] = 0,
