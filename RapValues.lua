c7f36145c93619c796bcf8b9d842d7804a2b9407
Trades <rapbot@example.com>
2026-05-18 03:25:00 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -1691,7 +1691,7 @@ return {
     ["Eggshell Shardblade_None"] = 5,
     ["Eggsplitter Blade_None"] = 3,
     ["Eggsplosive Confidence_None"] = 46,
-    ["Eggsplosive Exit_None"] = 625,
+    ["Eggsplosive Exit_None"] = 928,
     ["Eggstorm Swarm_None"] = 4,
     ["Eggtastrophe_None"] = 11,
     ["EGO_None"] = 31,
