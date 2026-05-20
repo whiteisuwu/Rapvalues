88fb71cb12c5203f792536653c079c61bf68ecca
Trades <rapbot@example.com>
2026-05-20 04:34:03 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -2613,7 +2613,7 @@ return {
     ["Jack-O-Lantern Blast_None"] = 9,
     ["Jack's Grin_None"] = 10,
     ["Jackolantern_Finisher"] = 22477,
-    ["Jackolantern_None"] = 11724,
+    ["Jackolantern_None"] = 14000,
     ["Jackpot Dance_None"] = 16,
     ["Jackpot Detonation_None"] = 6,
     ["Jade Branch Sword_None"] = 0,
