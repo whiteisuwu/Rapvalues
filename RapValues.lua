84b3e0250fcabda6ba109f44426db43243d5df9e
Trades <rapbot@example.com>
2026-05-20 12:47:31 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4144,7 +4144,7 @@ return {
     ["Spiked Club_None"] = 6,
     ["Spinalis Emote_None"] = 1104,
     ["Spinalis Explosion_None"] = 2417,
-    ["Spinalis_None"] = 4694,
+    ["Spinalis_None"] = 10000,
     ["Spine Chiller_None"] = 3,
     ["Spinjitzu [DUAL YIN YANG KATANA]_None"] = 23,
     ["Spinjitzu!_None"] = 24,
