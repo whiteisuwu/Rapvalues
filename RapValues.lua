0ef557c7839b665fa2932edecccaace8540992b5
Trades <rapbot@example.com>
2026-05-23 19:57:37 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3310,7 +3310,7 @@ return {
     ["Pink Healer_None"] = 68,
     ["Pink Ninja Katana Emote_None"] = 275,
     ["Pink Ninja Katana_Finisher"] = 19738,
-    ["Pink Ninja Katana_None"] = 4940,
+    ["Pink Ninja Katana_None"] = 6500,
     ["Pink Ninja Star Emote_None"] = 99,
     ["Pink Ninja Star Explosion_None"] = 346,
     ["Pink Ninja Star_None"] = 1616,
