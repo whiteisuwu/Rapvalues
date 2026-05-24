b26da06972d49b4b065b378641787cc43febd60f
Trades <rapbot@example.com>
2026-05-24 20:53:34 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4442,7 +4442,7 @@ return {
     ["Tiger Shark_None"] = 3,
     ["Tiger_None"] = 0,
     ["Tiger's Katana Emote_None"] = 980,
-    ["Tiger's Katana_None"] = 7000,
+    ["Tiger's Katana_None"] = 11187,
     ["TigerFX_None"] = 0,
     ["Tigershadow Blade_None"] = 4,
     ["Time Stopper_None"] = 4,
