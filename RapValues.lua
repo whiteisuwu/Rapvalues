db4e338547dce2f0bf620317060122b372a4a4a3
Trades <rapbot@example.com>
2026-05-25 13:20:35 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3032,7 +3032,7 @@ return {
     ["Necrotic Ruler_None"] = 423,
     ["Needle Connection Explosion_None"] = 118,
     ["Nemesis Blade_None"] = 5,
-    ["Neo-Neko Katana_None"] = 409,
+    ["Neo-Neko Katana_None"] = 250,
     ["Neo-Neko Needle_None"] = 138,
     ["Neon Burst_None"] = 6,
     ["Neon Cutter_None"] = 3,
