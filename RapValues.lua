2556cb6b9df8c351079f10478c6701bd162b8dba
Trades <rapbot@example.com>
2026-05-26 13:55:54 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3407,7 +3407,7 @@ return {
     ["Prince Blade_None"] = 2156,
     ["Princess Explosion_None"] = 49,
     ["Princess Katana_None"] = 2204,
-    ["Prismatic Cloud Rain_None"] = 900,
+    ["Prismatic Cloud Rain_None"] = 1848,
     ["Prismatic Explosion_None"] = 24,
     ["Prismatic Gem Blade_None"] = 0,
     ["Prismatic Gem Scythe_None"] = 0,
