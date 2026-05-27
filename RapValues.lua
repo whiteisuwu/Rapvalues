26e2a3418f934f4c90dd6df32551f4f3d6a64a80
Trades <rapbot@example.com>
2026-05-27 14:30:07 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -2904,7 +2904,7 @@ return {
     ["Melon Slice_None"] = 2,
     ["Meltinglight_None"] = 5,
     ["Meme Destroyer_None"] = 47,
-    ["Menacing_None"] = 501,
+    ["Menacing_None"] = 350,
     ["Meowplosion_None"] = 1276,
     ["Meowstruck_None"] = 1214,
     ["Mermaid's Blessing_None"] = 13,
