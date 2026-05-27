b6a6bd11247b46db30ceaeee28070f68cc7222ee
Trades <rapbot@example.com>
2026-05-27 22:41:19 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3241,7 +3241,7 @@ return {
     ["Peachborne Fan Emote_None"] = 89,
     ["Peachborne Fan_None"] = 939,
     ["Pearl Angel Katana Emote_None"] = 298,
-    ["Pearl Angel Katana Explosion_None"] = 210,
+    ["Pearl Angel Katana Explosion_None"] = 412,
     ["Pearl Angel Katana_None"] = 2900,
     ["Pearl Dagger_None"] = 8,
     ["Pearl Shard_None"] = 94,
