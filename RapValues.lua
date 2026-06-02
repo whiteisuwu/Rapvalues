0f711692ffc557ae1d5041e34cebc6d113a6f511
Trades <rapbot@example.com>
2026-06-02 10:54:14 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4102,7 +4102,7 @@ return {
     ["Sonic Boom_None"] = 5,
     ["SorceryRig_None"] = 0,
     ["Soul Blade_None"] = 6,
-    ["Soul Counter_None"] = 800,
+    ["Soul Counter_None"] = 1219,
     ["Soul Dance_None"] = 8,
     ["Soul Drain_None"] = 19,
     ["Soul Lantern_None"] = 263,
