8d103a9db52c92723d8ef851f010e2b91983346b
Trades <rapbot@example.com>
2026-06-11 21:25:23 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4560,7 +4560,7 @@ return {
     ["Unlikely Foe_None"] = 8,
     ["Unstable Blade_None"] = 5,
     ["Uptown Downtown_None"] = 6,
-    ["Valentine Hearts_None"] = 8200,
+    ["Valentine Hearts_None"] = 7214,
     ["Valentine Love_None"] = 0,
     ["Valkyrien Blade_None"] = 98,
     ["Valkyrien Eyes_None"] = 33,
