60c5c96760e975fba9e44fe18a15ff45ac8bef5c
Trades <rapbot@example.com>
2026-06-18 23:18:27 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -744,7 +744,7 @@ return {
     ["Cloud Rapier_None"] = 3,
     ["Cloud Summon_None"] = 125375,
     ["Cloud Sword_None"] = 129,
-    ["Cloud_None"] = 12684,
+    ["Cloud_None"] = 18000,
     ["CloudRig_None"] = 0,
     ["CLOUDSTARS_None"] = 0,
     ["Coal_None"] = 3,
