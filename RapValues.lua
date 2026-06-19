a41c69b380bc2439f76a14dc9b0f9f97cb8d8ed6
Trades <rapbot@example.com>
2026-06-19 07:26:05 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -491,7 +491,7 @@ return {
     ["Boom Shaka Laka_None"] = 8,
     ["Boost Me Up!_None"] = 11,
     ["Boreal Frostreaver_None"] = 1291,
-    ["Borealis_Finisher"] = 40000,
+    ["Borealis_Finisher"] = 35006,
     ["Borealis_None"] = 24279,
     ["Boundless Reaver_None"] = 5,
     ["Bounty Claimed_None"] = 184,
