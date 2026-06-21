7d90e20f450cac2463b06902fe29b4ad32d6383f
Trades <rapbot@example.com>
2026-06-21 08:17:02 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -1790,7 +1790,7 @@ return {
     ["Eternal Edge_None"] = 6,
     ["Eternal Ice_None"] = 11,
     ["Eternal Nightmare_None"] = 1155,
-    ["Eternal Piercer_None"] = 23000,
+    ["Eternal Piercer_None"] = 22906,
     ["Eternal Rupture_None"] = 6,
     ["Eternal Scythe_Finisher"] = 47069,
     ["Eternal Scythe_None"] = 2756,
