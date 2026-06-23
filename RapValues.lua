427f41fd1980afa6feed2f0688bef550437ba918
Trades <rapbot@example.com>
2026-06-23 00:57:27 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3664,7 +3664,7 @@ return {
     ["Reindeer Hammer_None"] = 78,
     ["Reindeer Horn Dagger_None"] = 15,
     ["Reindeer Leap_None"] = 9,
-    ["Reindeer_None"] = 25974,
+    ["Reindeer_None"] = 29500,
     ["Reindeer_SwordAccessory"] = 60436,
     ["Rekindled Gladius_None"] = 5,
     ["Rekindled_None"] = 7,
