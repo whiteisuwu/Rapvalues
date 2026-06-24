85cc0d9d4d4c90b4744344c734a2db4af0fb775c
Trades <rapbot@example.com>
2026-06-24 09:30:00 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3465,7 +3465,7 @@ return {
     ["Pumpkin Throw_None"] = 9,
     ["Pumpkin Toss_None"] = 4,
     ["Pumpkin VineCrusher_None"] = 6,
-    ["Puppy_None"] = 23129,
+    ["Puppy_None"] = 25000,
     ["Puppy_Plushie_Rig2_None"] = 0,
     ["Pure Power_None"] = 81,
     ["Pure Silk Explosion_None"] = 87,
