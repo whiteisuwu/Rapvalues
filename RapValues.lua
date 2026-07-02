b553d71d748aef9191a4ae15ed6518f1c884e710
Trades <rapbot@example.com>
2026-07-02 08:33:15 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -527,7 +527,7 @@ return {
     ["Bronze Sword_Sword_None_Bronze Sword"] = 6,
     ["BRR BRR PATAPIMPLOSION_Explosion_None_BRR BRR PATAPIMPLOSION"] = 5,
     ["Brutality Affection Bat Emote_Emote_None_1222"] = 442,
-    ["Brutality Affection Bat_Sword_None_Brutality Affection Bat"] = 6094,
+    ["Brutality Affection Bat_Sword_None_Brutality Affection Bat"] = 5200,
     ["Brutality Affection Explosion_Explosion_None_Brutality Affection Explosion"] = 395,
     ["Bubble Blade_Sword_None_Bubble Blade"] = 342,
     ["Bubble Blaster Emote_Emote_None_639"] = 113,
