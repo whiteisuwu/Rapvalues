0a3f520d0ca6c32cd72b96c635b02f3473183b26
Trades <rapbot@example.com>
2026-07-04 17:35:39 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -2785,7 +2785,7 @@ return {
     ["Kitty Katana Explosion_Explosion_None_Kitty Katana Explosion"] = 1886,
     ["Kitty Katana_Sword_None_Kitty Katana"] = 8875,
     ["Kitty Launcher Emote_Emote_None_554"] = 2362,
-    ["Kitty Launcher_Sword_None_Kitty Launcher"] = 18000,
+    ["Kitty Launcher_Sword_None_Kitty Launcher"] = 16457,
     ["Kitty Rocket_Explosion_None_Kitty Rocket"] = 9027,
     ["Knee Slide_Emote_None_1241"] = 0,
     ["Knight's Crest_Sword_None_Knight's Crest"] = 11,
