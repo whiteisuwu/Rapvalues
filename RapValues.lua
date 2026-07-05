d69cb5464f386bbedee007c2b689a0e5a7a96c33
Trades <rapbot@example.com>
2026-07-05 09:53:31 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4925,7 +4925,7 @@ return {
     ["Winter Wizard_Emote_None_593"] = 9,
     ["Winter Wolf Emote_Emote_None_1116"] = 1223,
     ["Winter Wolf Explosion_Explosion_None_Winter Wolf Explosion"] = 1881,
-    ["Winter Wolf_Sword_None_Winter Wolf"] = 18000,
+    ["Winter Wolf_Sword_None_Winter Wolf"] = 15638,
     ["Winter Wolf_Sword_SwordAccessory_Winter Wolf"] = 41068,
     ["Winter Wreath Toss_Emote_None_685"] = 4,
     ["Winter's Aurora_Sword_None_Winter's Aurora"] = 14,
