dd2427a29df7429fa59d359707e4653afcee0901
Trades <rapbot@example.com>
2026-07-12 14:33:23 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -2918,7 +2918,7 @@ return {
     ["Lotus Meditation_Emote_None_136"] = 18,
     ["Love Blade_Sword_None_Love Blade"] = 166,
     ["Love Bow_Explosion_None_Love Bow"] = 121,
-    ["Love For You_Emote_None_1167"] = 9500,
+    ["Love For You_Emote_None_1167"] = 6932,
     ["Love Letter_Emote_None_778"] = 11,
     ["Love Pop_Explosion_None_Love Pop"] = 61,
     ["Love Sickle_Sword_None_Love Sickle"] = 162,
