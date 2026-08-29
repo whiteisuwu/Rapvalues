c7dac877c68f1bad209484930f6cc9048ccbc87c
Trades <rapbot@example.com>
2026-08-29 18:44:12 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -1862,7 +1862,7 @@ return {
     ["Emerald Cyclone_Sword_None_Emerald Cyclone"] = 5,
     ["Emerald Edge_Sword_None_Emerald Edge"] = 11,
     ["Emerald Eggblade_Sword_None_Emerald Eggblade"] = 12,
-    ["Emerald Greatsword_Sword_None_Emerald Greatsword"] = 1200,
+    ["Emerald Greatsword_Sword_None_Emerald Greatsword"] = 12335,
     ["Emerald Halo_Explosion_None_Emerald Halo"] = 55,
     ["Emerald Katana_Sword_None_Emerald Katana"] = 36,
     ["Emerald Parasol Emote_Emote_None_896"] = 64,
