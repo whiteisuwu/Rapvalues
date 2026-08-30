7cdcc0ca0121b04aa39d6d6a5b95e357446b9ee3
Trades <rapbot@example.com>
2026-08-30 02:44:30 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -644,7 +644,7 @@ return {
     ["Celestial Spear_Sword_None_Celestial Spear"] = 18565,
     ["Celestial Staff_Sword_None_Celestial Staff"] = 2246,
     ["Celestial Vanguard_Sword_None_Celestial Vanguard"] = 9,
-    ["Celestial Whisper_Sword_None_Celestial Whisper"] = 21311,
+    ["Celestial Whisper_Sword_None_Celestial Whisper"] = 22000,
     ["Cerberus_Sword_None_Cerberus"] = 5,
     ["Cerberus' Fang_Sword_None_Cerberus' Fang"] = 4,
     ["Cha Cha Slide_Emote_None_945"] = 19,
