30748af3122baa41f44728261a371ebbc9165527
Trades <rapbot@example.com>
2026-09-07 15:16:45 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -5174,7 +5174,7 @@ return {
     ["Zombie Movement_Emote_None_567"] = 0,
     ["Zombie Run_Emote_None_107"] = 6,
     ["Zombie Shake_Emote_None_106"] = 7,
-    ["Zombie Slide_Emote_None_12"] = 105000,
+    ["Zombie Slide_Emote_None_12"] = 102128,
     ["Zombie Sword_Sword_None_Zombie Sword"] = 4,
     ["Zombie Walk_Emote_None_39"] = 15,
     ["Zoru_Sword_None_Zoru"] = 3,
