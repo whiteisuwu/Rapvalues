1c1e7c45f30ce6f43c80e8ab056a859fa1ed44ec
Trades <rapbot@example.com>
2026-09-08 12:32:25 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -443,7 +443,7 @@ return {
     ["Bloodthirster_Sword_None_Bloodthirster"] = 9,
     ["Bloodthirster's Embrace_Explosion_None_Bloodthirster's Embrace"] = 5,
     ["Bloom Awakening_Explosion_None_Bloom Awakening"] = 386,
-    ["Bloom Katana_Sword_None_Bloom Katana"] = 3353,
+    ["Bloom Katana_Sword_None_Bloom Katana"] = 300,
     ["Bloom Shuriken_Sword_Finisher_Bloom Shuriken"] = 87674,
     ["Bloom Shuriken_Sword_None_Bloom Shuriken"] = 63749,
     ["Bloomfire_Explosion_None_Bloomfire"] = 33,
