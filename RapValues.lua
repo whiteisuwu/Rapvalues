bcfdb3e7cf21d2f4b8f90ca197325598081872fc
Trades <rapbot@example.com>
2026-09-18 21:43:30 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4457,7 +4457,7 @@ return {
     ["Spring Saber_Sword_None_Spring Saber"] = 167,
     ["Spring Scythe_Emote_None_1177"] = 58,
     ["Spring Scythe_Sword_None_Spring Scythe"] = 267,
-    ["Spring Slicer_Sword_None_Spring Slicer"] = 5594,
+    ["Spring Slicer_Sword_None_Spring Slicer"] = 1000,
     ["Spring Sun Bow_Sword_None_Spring Sun Bow"] = 410,
     ["Spring Sun Scythe_Sword_None_Spring Sun Scythe"] = 633,
     ["Spring's Guardian_Sword_None_Spring's Guardian"] = 3,
