b68f340821bbc104fe7fe642e2f006541009ff5e
Trades <rapbot@example.com>
2026-09-20 13:45:04 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -4051,7 +4051,7 @@ return {
     ["Samurai Tsujigiri_Explosion_None_Samurai Tsujigiri"] = 55,
     ["Samurai's Backblade_Sword_None_Samurai's Backblade"] = 17680,
     ["Samurai's Set Emote_Emote_None_937"] = 45,
-    ["Samurai's Set_Sword_Finisher_Samurai's Set"] = 1600,
+    ["Samurai's Set_Sword_Finisher_Samurai's Set"] = 2396,
     ["Samurai's Set_Sword_None_Samurai's Set"] = 649,
     ["Samurai's Sickle_Sword_None_Samurai's Sickle"] = 69,
     ["Sand Dagger_Sword_None_Sand Dagger"] = 3,
