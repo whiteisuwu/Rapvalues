3d48be6a845a3262b381857bdb007097fa46ed6e
Trades <rapbot@example.com>
2026-09-23 21:36:36 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -699,7 +699,7 @@ return {
     ["Chroma Blade Emote_Emote_None_348"] = 569,
     ["Chroma Blade Explosion_Explosion_None_Chroma Blade Explosion"] = 1392,
     ["Chroma Blade_Sword_Finisher_Chroma Blade"] = 49494,
-    ["Chroma Blade_Sword_None_Chroma Blade"] = 15563,
+    ["Chroma Blade_Sword_None_Chroma Blade"] = 16000,
     ["Chroma Blaster_Sword_None_Chroma Blaster"] = 163,
     ["Chroma Blossom_Explosion_None_Chroma Blossom"] = 9,
     ["Chroma Cards_Sword_Finisher_Chroma Cards"] = 1500000,
