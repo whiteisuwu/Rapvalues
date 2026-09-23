a0a89513be542819ede7fc5bdfdfeb10d6f59750
Trades <rapbot@example.com>
2026-09-23 21:36:36 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -3306,7 +3306,7 @@ return {
     ["Ninja Shuriken_Sword_None_Ninja Shuriken"] = 321,
     ["Ninja Training_Emote_None_942"] = 4,
     ["Ninja Wall Flip_Emote_None_939"] = 4,
-    ["NO BATIDÃO_Emote_None_1272"] = 4269,
+    ["NO BATIDÃO_Emote_None_1272"] = 3400,
     ["Noble Fang_Sword_None_Noble Fang"] = 4,
     ["Noise Maker_Emote_None_65"] = 43,
     ["NomNom_Emote_None_28"] = 61,
