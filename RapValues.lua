40589d4700791672a59496b2f31fe9ca123233b9
Trades <rapbot@example.com>
2026-09-24 05:36:50 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -369,7 +369,7 @@ return {
     ["Black Ninja Star_Sword_None_Black Ninja Star"] = 1979,
     ["Black Oni Katana Emote_Emote_None_971"] = 195,
     ["Black Oni Katana Explosion_Explosion_None_Black Oni Katana Explosion"] = 480,
-    ["Black Oni Katana_Sword_None_Black Oni Katana"] = 3500,
+    ["Black Oni Katana_Sword_None_Black Oni Katana"] = 4379,
     ["Black Splash_Explosion_None_Waterblast Black"] = 3,
     ["Blackhole Burst_Explosion_None_Blackhole Burst"] = 9,
     ["Blackhole Gauntlets_Sword_None_Blackhole Gauntlets"] = 566,
