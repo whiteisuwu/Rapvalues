0fbcbd650c38e8ee36406d751076e6a59739a787
Trades <rapbot@example.com>
2026-09-25 22:57:22 +0000

    Sync RAP values (Overrides injected)

--- RapValues.lua
+++ RapValues.lua
@@ -1058,7 +1058,7 @@ return {
     ["Deathpiercer_Sword_None_Deathpiercer"] = 0,
     ["Deathrider Explosion_Explosion_None_Deathrider Explosion"] = 1816,
     ["Deathrider_Emote_None_1269"] = 743,
-    ["Deathrider_Sword_None_Deathrider"] = 14000,
+    ["Deathrider_Sword_None_Deathrider"] = 11891,
     ["Deathscar Core_Explosion_None_Deathscar Core"] = 234,
     ["Deathscar Eye_Explosion_None_Deathscar Eye"] = 57,
     ["Deathwarden Blade_Sword_None_Deathwarden Blade"] = 176,
